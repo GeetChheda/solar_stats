299b425c667b12669e5a9693dd10145a31732491
GeetChheda <gac222@cornell.edu>
2023-12-15 17:40:41 +0000

    ....

--- z/workflow.R
+++ z/workflow.R
@@ -5,10 +5,20 @@ source("R/generate_stats_plots.R")
 # Or use load_all() from devtools to load them as if they were a package
 # devtools::load_all(".")
 
-# Test out making new functions
+# Testing of functions
+
+solardata1 = generate_stats_plot("z/station00.csv",
+                                "date_time", "power", spec = 1.297, 
+                                "Weeks (Subgroups)", "Average Power (KWh)",
+                                "Variation of Average Power against Time")
 
 
-# When you're happy with a function, go put it in an R script in the /R folder.
+## This below fucntion should show an error as the input spec is missing
+solardata2 = generate_stats_plot("z/station00.csv",
+                                "date_time", "power",
+                                "Weeks (Subgroups)", "Average Power (KWh)",
+                                "Variation of Average Power against Time")
+
 
 # Always a good idea to clear your environment and cache
 rm(list = ls()); gc()
